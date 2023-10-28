cfd220e927a2e0a3dd02e37714596d2c5b24a260
David De Anda <omarandstuff@gmail.com>
2023-10-27 20:20:50 -0600

    Test using testing tools

--- tests/ExpressFileUpload.module.test.ts
+++ tests/ExpressFileUpload.module.test.ts
@@ -1,13 +1,16 @@
-import { Logger } from '@universal-packages/logger'
-
 import { ExpressFileUploadModule } from '../src'
+
+jestCore.runBare({
+  coreConfigOverride: {
+    config: { location: './tests/__fixtures__/config' },
+    modules: { location: './tests/__fixtures__' },
+    logger: { silence: true }
+  }
+})
 
 describe(ExpressFileUploadModule, (): void => {
   it('behaves as expected', async (): Promise<void> => {
-    const logger = new Logger({ silence: true })
-    const module = new ExpressFileUploadModule({} as any, logger)
-
-    await module.prepare()
-    await module.release()
+    expect(core.coreModules.expressFileuploadModule).not.toBeUndefined()
+    expect(core.coreModules.expressFileuploadModule.config).toEqual({ tempFileDir: './tmp1' })
   })
 })
---
+++ tests/__fixtures__/ExpressFileUpload.task.ts
@@ -0,0 +1,1 @@
+export { default } from '../../src/ExpressFileUpload.universal-core-task'
