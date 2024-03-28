3421b574bbb60c483441088b1a2160bdd7c0e928
David De Anda <omarandstuff@gmail.com>
2024-03-28 12:00:18 -0600

    Upgrade to new logger usage

--- src/ExpressFileUpload.universal-core-task.ts
+++ src/ExpressFileUpload.universal-core-task.ts
@@ -11,7 +11,7 @@ export default class ExpressFileUploadTask extends CoreTask {
       case 'init':
         await populateTemplates(path.resolve(__dirname, 'template'), './src', { override: this.args.f })
 
-        this.logger.publish('INFO', 'Express fileupload template initialized')
+        this.logger.log({ level: 'INFO', title: 'Express fileupload template initialized' })
         break
       default:
         throw new Error(`Unrecognized directive ${this.directive}`)
