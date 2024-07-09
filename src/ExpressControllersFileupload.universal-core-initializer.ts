4069023c41d874f6421d736ebcdbfea556125957
David De Anda <omarandstuff@gmail.com>
2024-07-08 19:29:54 -0600

    Initializer in favor of custom task finishing

--- src/ExpressControllersFileupload.universal-core-initializer.ts
+++ src/ExpressControllersFileupload.universal-core-initializer.ts
@@ -2,6 +2,7 @@ import CoreInitializer from '@universal-packages/core/CoreInitializer'
 
 export default class ExpressControllersFileuploadInitializer extends CoreInitializer {
   public static readonly initializerName = 'express-controllers-fileupload'
+    public static readonly description: string = 'Express Controllers Fileupload Initializer'
 
   public readonly templatesLocation: string = `${__dirname}/templates`
 }
