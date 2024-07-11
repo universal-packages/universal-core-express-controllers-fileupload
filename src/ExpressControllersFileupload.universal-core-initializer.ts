903776e0882b224802a22ce4f9cf3e7b32d9c6ad
David De Anda <omarandstuff@gmail.com>
2024-07-11 15:06:15 -0600

    Right initializer import

--- src/ExpressControllersFileupload.universal-core-initializer.ts
+++ src/ExpressControllersFileupload.universal-core-initializer.ts
@@ -1,4 +1,4 @@
-import CoreInitializer from '@universal-packages/core/CoreInitializer'
+import { CoreInitializer } from '@universal-packages/core'
 
 export default class ExpressControllersFileuploadInitializer extends CoreInitializer {
   public static readonly initializerName = 'express-controllers-fileupload'
